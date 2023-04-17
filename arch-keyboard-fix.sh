951f5b2864d1b725910228ba1c6cbb496f9ca20b
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2023-04-17 17:20:57 +0100

    Update arch-keyboard-fix.sh

--- arch-keyboard-fix.sh
+++ arch-keyboard-fix.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
-sudo mv reset-input-devices.sh /usr/local/bin/
-sudo mv reset-input-devices-after-sleep.service /etc/systemd/system/
+sudo cp reset-input-devices.sh /usr/local/bin/
+sudo cp reset-input-devices-after-sleep.service /etc/systemd/system/
 
 sudo chown root:root /usr/local/bin/reset-input-devices.sh
 sudo chmod 744 /usr/local/bin/reset-input-devices.sh
 
 systemctl enable --now /etc/systemd/system/reset-input-devices-after-sleep.service
+
+echo "Fix complete!"
