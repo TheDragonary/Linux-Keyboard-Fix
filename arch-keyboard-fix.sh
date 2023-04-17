f3e9e884815a07cdbc65e22bfe2d8a4ae93757c3
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2023-04-17 17:12:18 +0100

    Add files via upload

---
+++ arch-keyboard-fix.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+sudo mv reset-input-devices.sh /usr/local/bin/
+sudo mv reset-input-devices-after-sleep.service /etc/systemd/system/
+
+sudo chown root:root /usr/local/bin/reset-input-devices.sh
+sudo chmod 744 /usr/local/bin/reset-input-devices.sh
+
+systemctl enable --now /etc/systemd/system/reset-input-devices-after-sleep.service
---
+++ reset-input-devices.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+# Reset the keyboard driver and USB mouse 
+        
+modprobe -r atkbd
+modprobe atkbd reset=1
+echo "Finished resetting the keyboard."
+        
+# Reset every USB device, because we don't know in advance which port
+# the mouse is plugged into. Send errors to /dev/null to avoid 
+# cluttering up the logs.
+for USB in /sys/bus/usb/devices/*/authorized; do
+    eval "echo 0 > $USB" 2>/dev/null 
+    eval "echo 1 > $USB" 2>/dev/null
+done
+echo "Finished resetting USB inputs."
